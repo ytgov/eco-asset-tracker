afb451a24539d0775c91f6d9c518319edc533aab
Ryan Agar <ryanjagar@hey.com>
2022-11-23 09:30:13 -0700

    join tables for personel room and key

---
+++ api/src/data/migrations/008_personnel_key.ts
@@ -0,0 +1,22 @@
+import * as knex from "knex";
+
+exports.up = function (knex: knex.Knex, Promise: any) {
+  console.log("Creating personnel to key join table...");
+  return knex.schema.createTable("personnel_key", function (table) {
+    table.increments("_id").primary();
+    table.string("personnel_id");
+    table.string("key_id");
+    // table.string("personnel_id").references("personnel._id"); //foreign key to personnel
+    // table.string("room_id").references("rooms._id"); //foreign key to room
+
+    //audit
+    table.date("modified");
+    table.string("modified_by");
+    table.date("created_at");
+  });
+};
+
+exports.down = function (knex: knex.Knex, Promise: any) {
+  console.log("Dropping personnel to key join table");
+  return knex.schema.dropTable("personnel_key");
+};
---
+++ api/src/data/migrations/009_room_key.ts
@@ -0,0 +1,22 @@
+import * as knex from "knex";
+
+exports.up = function (knex: knex.Knex, Promise: any) {
+  console.log("Creating room to key join table...");
+  return knex.schema.createTable("room_key", function (table) {
+    table.increments("_id").primary();
+    table.string("room_id");
+    table.string("key_id");
+    // table.string("personnel_id").references("personnel._id"); //foreign key to personnel
+    // table.string("room_id").references("rooms._id"); //foreign key to room
+
+    //audit
+    table.date("modified");
+    table.string("modified_by");
+    table.date("created_at");
+  });
+};
+
+exports.down = function (knex: knex.Knex, Promise: any) {
+  console.log("Dropping room to key join table");
+  return knex.schema.dropTable("room_key");
+};
