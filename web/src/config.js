a76e9a865ad8a33f34172c78b6d5691a4644862f
Max Parker <maxrparker@gmail.com>
2023-08-08 15:28:51 -0700

    version bump

--- web/src/config.js
+++ web/src/config.js
@@ -7,20 +7,15 @@ export const sections = [
   {
     name: "Dashboard",
     url: "/dashboard",
-    icon: "mdi-view-dashboard",
+    icon: "mdi-view-dashboard"
   },
   {
     name: "Vendors",
     url: "/vendors",
-    icon: "mdi-domain",
-  },
+    icon: "mdi-domain"
+  }
 ];
 export const environment = process.env.NODE_ENV;
-export const apiBaseUrl =
-  process.env.NODE_ENV == "production" ? "" : "http://localhost:3000";
-export const applicationUrl =
-  process.env.VUE_APP_FRONTEND_URL || "http://localhost:8080";
-export const apiConfigUrl =
-  process.env.NODE_ENV == "production"
-    ? "/api/config"
-    : "http://localhost:3000/api/config";
+export const apiBaseUrl = process.env.NODE_ENV == "production" ? "" : "http://localhost:3000";
+export const applicationUrl = process.env.VUE_APP_FRONTEND_URL || "http://localhost:8080";
+export const apiConfigUrl = process.env.NODE_ENV == "production" ? "/api/config" : "http://localhost:3000/api/config";
