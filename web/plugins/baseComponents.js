c28293a4c9e890a776b6f8663bb005a59f04dd04
Ryan Agar <ryanjagar@hey.com>
2023-12-28 08:13:10 -0700

    add missing package

--- web/plugins/baseComponents.js
+++ web/plugins/baseComponents.js
@@ -1,6 +1,8 @@
 const components = import.meta.glob("@/components/base/*.vue", { eager: true });
+import JsonCSV from "vue-json-csv";
 
 const register = (app) => {
+  app.component("downloadCsv", JsonCSV);
   Object.entries(components).forEach(([path, definition]) => {
     // Get name of component, based on filename
     // "./components/Fruits.vue" will become "Fruits"
