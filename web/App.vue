8b89aefd889e09806ae551700838b1e20ebf0391
Ryan Agar <ryanjagar@hey.com>
2024-02-18 10:04:32 -0700

    Refactor App.vue component

--- web/App.vue
+++ web/App.vue
@@ -1,8 +1,14 @@
 <template>
   <v-app>
-    <v-overlay persistent :model-value="!isInitialized" class="align-center justify-center">
+    <v-overlay
+      persistent
+      :model-value="!isInitialized"
+      class="align-center justify-center">
       <div class="text-center pt-15 my-10">
-        <v-progress-circular indeterminate size="64" color="white" />
+        <v-progress-circular
+          indeterminate
+          size="64"
+          color="white" />
       </div>
 
       <h2 class="text-white text my-10">Loading Application...</h2>
@@ -19,41 +25,10 @@
   </v-app>
 </template>
 
-<script setup>
-import { useAuth0 } from "@auth0/auth0-vue";
-// import { auth0 } from "@/plugins/auth0";
-import { onMounted } from "vue";
-
-const auth0 = useAuth0();
-
-async function login() {
-  await auth0.loginWithRedirect().then(() => {
-    if (auth0.isAuthenticated) {
-      console.log("App.vue: User is authenticated");
-      auth0.getAccessTokenSilently().then((token) => {
-        console.log("token: " + token);
-      });
-    }
-
-  });
-}
-
-onMounted(async () => {
-
-  if (!auth0.isAuthenticated) {
-    await login();
-
-  }
-  console.log(auth0.isAuthenticated.value);
-  console.log(auth0.user.value);
-});
-
-// const user = computed(() => auth0);
-</script>
-
 <script>
 // import { mapActions, mapState } from "pinia";
 // import { useAuthStore } from "@/stores/AuthStore";
+import { mapActions } from "vuex";
 
 export default {
   name: "App",
@@ -75,7 +50,9 @@ export default {
     //   }
     // },
   },
-  async mounted() { },
+  async mounted() {
+    await this.initialize();
+  },
   async created() {
     // if (!this.isInitialized) {
     //   console.log("App.vue: App is not initialized");
@@ -86,7 +63,7 @@ export default {
   },
 
   methods: {
-    // ...mapActions(useAuthStore, ["initialize"]),
+    ...mapActions(["initialize"]),
     doNothing: function () {
       return null;
     },
@@ -102,11 +79,4 @@ export default {
 </script>
 <style>
 @import "./assets/yk-style.css";
-
-/* html {
-  background: url(./assets/SOMETLogo1920x1080.jpg) no-repeat center center fixed;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  background-size: cover;
-} */
 </style>
