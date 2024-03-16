458ba529b0d81c5f6fc276a9fb7eaa7b16389dec
Ryan Agar <ryanjagar@hey.com>
2024-03-16 11:49:33 -0700

    🛂 router changes to support new auth flow

--- web/router.js
+++ web/router.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
-// import { authGuard } from "@auth0/auth0-vue";
+import { authGuard } from "@auth0/auth0-vue";
+//If you need to manipualte auth elements directly use:
+// import { auth0 } from "@/plugins/auth";
 import homeRoutes from "@/modules/home/router";
 import keyRoutes from "@/modules/keys/router";
 import roomsRoutes from "@/modules/rooms/router";
@@ -7,31 +9,19 @@ import assetRoutes from "@/modules/assets/router";
 import personnelRoutes from "@/modules/personnel/router";
 import administrationRoutes from "@/modules/administration/router";
 
-// import { useAuthStore } from "@/stores/AuthStore";
-// import myStatusRoutes from "@/modules/mystatus/router";
-// import rosterRoutes from "@/modules/roster/router";
-// import pagingRouter from "@/modules/paging/router";
-// import adminRoutes from "@/modules/admin/router";
-// import eventsRoutes from "@/modules/events/router";
-
-// import homeRoutes from "@/modules/home/router";
 const routes = [
-  // {
-  //   path: "/login",
-  //   name: "Login",
-  //   component: () => import("@/modules/login/views/Login.vue"),
-  // },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("@/views/Login.vue"),
+  },
   {
     path: "",
-    // name: "Home",
+    name: "home",
+
     redirect: { name: "Dashboard" },
-    // beforeEnter: authGuard,
-    // component: () => import("@/views/Main.vue"),
-    // redirect: "/status",
-    // component: () => import("@/layouts/Layout.vue"),
-    // meta: {
-    //   requiresAuth: true,
-    // },
+    beforeEnter: authGuard,
+
     children: [
       ...homeRoutes,
       // ...authenticationRoutes,
@@ -41,6 +31,11 @@ const routes = [
       ...keyRoutes,
       ...personnelRoutes,
     ],
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("@/views/Login.vue"),
   },
   {
     path: "/page-not-found",
@@ -62,22 +57,7 @@ router.beforeEach(async (to, from, next) => {
       // query: { redirect: to.fullPath, redirect: ""
     });
   }
-  // const authStore = useAuthStore();
-  // if (to.matched.some((record) => record.meta.requiresAuth)) {
-  //   if (!authStore.isInitialized) {
-  //     await authStore.initialize();
-  //   }
-  //   if (!authStore.isAuthenticated) {
-  //     next({
-  //       path: "/login",
-  //       query: { redirect: to.fullPath },
-  //     });
-  //   } else {
-  //     next();
-  //   }
-  // } else {
-  //   next();
-  // }
+
   next();
 });
 
