62acf53798baffc18d8fadd0995047978dd54265
Ryan Agar <ryanjagar@hey.com>
2024-03-23 09:13:14 -0700

    make login pretty

--- web/views/Login.vue
+++ web/views/Login.vue
@@ -1,18 +1,20 @@
 <template>
-  <v-container class="fill-height">
+  <v-container
+    fluid
+    class="fill-height login-background">
     <v-row justify="center">
-      <v-col sm="4">
-        <v-toolbar
-          dark
-          color="secondary"
-          flat>
-          <v-toolbar-title class="no-icon-space text-center">{{
-            applicationName
-          }}</v-toolbar-title>
-        </v-toolbar>
+      <v-col sm="3">
         <v-card
           rounded="4"
-          class="elevation-12 text-center">
+          class="elevation-12 text-center no-border">
+          <v-toolbar
+            dark
+            color="secondary"
+            flat>
+            <v-toolbar-title class="no-icon-space text-center">{{
+              applicationName
+            }}</v-toolbar-title>
+          </v-toolbar>
           <v-card-text> <v-btn @click="doLogin">Login</v-btn> </v-card-text>
         </v-card>
       </v-col>
@@ -46,7 +48,17 @@ export default {
 </script>
 
 <style scoped>
+.no-border {
+  border: 0px !important;
+}
 .no-icon-space {
   margin-left: 0 !important;
+}
+.login-background {
+  background: url("@/assets/images/kristina-delp-CoGyhon1Q1E-unsplash.jpg")
+    no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  background-size: cover;
 }
 </style>
