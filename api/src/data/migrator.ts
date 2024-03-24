6a212b75bfc62e9dc5dd85a2d6f26e95a4824b4f
Ryan Agar <ryanjagar@hey.com>
2024-03-24 10:52:36 -0700

    Refactor migration functions and add migration status

--- api/src/data/migrator.ts
+++ api/src/data/migrator.ts
@@ -4,35 +4,51 @@ import { join } from "path";
 
 const db = new KnexService().db;
 
+const migrationConfig = {
+  directory: join(__dirname, "migrations"),
+};
+
 export async function migrateUp() {
-    console.log("-------- MIGRATE UP ---------")
-    console.log(join(__dirname, "migrations"))
-    return await db.migrate.up({ directory: join(__dirname, "migrations") });
+  console.log("-------- MIGRATE UP ---------");
+
+  try {
+    return await db.migrate.up(migrationConfig);
+  } catch (error) {
+    console.log("Something went wrong");
+    console.log(error);
+    return error;
+  }
 }
 
 export async function migrateDown() {
-    console.log("-------- MIGRATE DOWN ---------")
-    console.log(join(__dirname, "migrations"))
-    return await db.migrate.down({ directory: join(__dirname, "migrations") });
+  console.log("-------- MIGRATE DOWN ---------");
+  return await db.migrate.down({
+    migrationConfig,
+  });
 }
 
 export async function migrateLatest() {
-    console.log("-------- MIGRATE LATEST ---------")
-    return await db.migrate.latest({ directory: join(__dirname, "migrations") });
+  console.log("-------- MIGRATE LATEST ---------");
+  return await db.migrate.latest({
+    migrationConfig,
+  });
 }
 
-export async function CreateMigrationRoutes(app: Express) {
-
-    app.post("/migrate/up", async (req: Request, res: Response) => {
-        res.send(await migrateUp());
-    });
-
-    app.post("/migrate/down", async (req: Request, res: Response) => {
-        res.send(await migrateDown());
-    });
-
-    app.post("/migrate/latest", async (req: Request, res: Response) => {
-        console.log("Runing Latest Migrations")
-        res.send(await migrateLatest());
-    });
+export async function migrationStatus() {
+  return await db.migrate.list(migrationConfig);
 }
+
+// export async function CreateMigrationRoutes(app: Express) {
+//   app.post("/migrate/up", async (req: Request, res: Response) => {
+//     res.send(await migrateUp());
+//   });
+
+//   app.post("/migrate/down", async (req: Request, res: Response) => {
+//     res.send(await migrateDown());
+//   });
+
+//   app.post("/migrate/latest", async (req: Request, res: Response) => {
+//     console.log("Runing Latest Migrations");
+//     res.send(await migrateLatest());
+//   });
+// }
